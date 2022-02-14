f34502e054fa34fe6b97be9f94ffada5146f5b3e
Hans Berndl <hberndl70@gmail.com>
2022-02-14 15:52:36 +0100

    edx generator

--- run_generator.sh
+++ run_generator.sh
@@ -2,7 +2,7 @@
 # Script to execute Edx Generator
 
 edx_generator_script="./edx_generator.py"
-input="./test_course/input"
-output="./test_course/output"
+input="./input"
+output="./output"
 
 python3 $edx_generator_script $input $output
