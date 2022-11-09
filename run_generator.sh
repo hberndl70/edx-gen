5572a33f4afb573fe9c062e02f63db11d6cac1b0
Hans Berndl <hberndl70@gmail.com>
2022-11-09 16:30:11 +0100

    updates

--- run_generator.sh
+++ run_generator.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Script to execute edX Generator
 
-edx_generator_script="./edx_generator.py"
+edx_gen="./edx_generator.py"
 input="./input"
 output="./output"
 
-python3 $edx_generator_script $input $output
+python3 $edx_gen $input $output
